8eb823a8a753317e768096eaba6b2dc61cb70efa
Prasad1918 <prasadrelangi18@gmail.com>
2022-11-27 11:53:00 +0530

    first commit

---
+++ src/main/java/com/atm/i/service/JavaClass.java
@@ -0,0 +1,10 @@
+package com.atm.i.service;
+
+public class JavaClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/com/example/demo/Demo1SpBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1SpBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1SpBootApplication.class, args);
+	}
+
+}
